ad6b9d3ac0ddd0819154bed75dd37b444f03268f
schlanqe <ilhanburcakbas@gmail.com>
2023-09-23 12:39:49 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # han
+
+## Features
+
+* Minimalist prompt
+* The most compact vi mode support
+* Error indicator on return code != 0
+* Git support
+
+## Configuration
+* Set the following variable in your `~/.config/fish/config.fish` in order to use han.
+```fish
+fish_vi_mode
+```
--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -2,8 +2,8 @@ function han_status
     set -l last_status $status
     if not test $last_status -eq 0
         set_color --bold red
-        # echo "[!]"
-        echo ""
+        echo "!"
+        # echo ""
         set_color normal
     end
 end
@@ -13,5 +13,3 @@ function fish_right_prompt
     han_status
     set_color normal
 end
-
-  
